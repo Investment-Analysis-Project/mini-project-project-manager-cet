361235d1f7337d3e5e1a394c475c88fb4ef1a160
Govind V R <govindvr02@gmail.com>
2023-05-29 23:36:31 +0530

    refactor: minor changes

--- server/src/db/db.sql
+++ server/src/db/db.sql
@@ -1,31 +1,38 @@
 create database projectmanager;
 
-create table projects(
-    p_id VARCHAR(10) PRIMARY KEY NOT NULL,
-    p_name VARCHAR(50) NOT NULL,
-    p_desc TEXT,
-    team_id VARCHAR(50) NOT NULL,
-    domain VARCHAR(20) NOT NULL,
-    completed BOOLEAN NOT NULL,
-    abstract TEXT
+CREATE TABLE ProjectDisplay (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  program VARCHAR(100) NOT NULL,
+  department VARCHAR(100) NOT NULL,
+  year YEAR NOT NULL,
+  guide VARCHAR(64),
+  member1 VARCHAR(64),
+  member2 VARCHAR(64),
+  member3 VARCHAR(64),
+  member4 VARCHAR(64),
+  abstract TEXT,
+  report TEXT,
+  hosted_link VARCHAR(255),
+  code TEXT
 );
 
-create table teams(
-    t_id VARCHAR(10) PRIMARY KEY NOT NULL,
-    guide_id VARCHAR(10) NOT NULL,
-    program VARCHAR(10) NOT NULL,
-    grad_year varchar(4) NOT NULL
-);
+-- create table teams(
+--     t_id VARCHAR(10) PRIMARY KEY NOT NULL,
+--     guide_id VARCHAR(10) NOT NULL,
+--     program VARCHAR(10) NOT NULL,
+--     grad_year varchar(4) NOT NULL
+-- );
 
-create table guides(
-    g_id VARCHAR(10) PRIMARY KEY NOT NULL,
-    name VARCHAR(20) NOT NULL,
-    areas_of_interest  VARCHAR[]
-); 
+-- create table guides(
+--     g_id VARCHAR(10) PRIMARY KEY NOT NULL,
+--     name VARCHAR(20) NOT NULL,
+--     areas_of_interest  VARCHAR[]
+-- ); 
 
-create table students(
-    s_id VARCHAR(10) PRIMARY KEY NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    team_id VARCHAR(10) NOT NULL,
-    FOREIGN KEY(team_id) REFERENCES teams(t_id)
-);
+-- create table students(
+--     s_id VARCHAR(10) PRIMARY KEY NOT NULL,
+--     name VARCHAR(50) NOT NULL,
+--     team_id VARCHAR(10) NOT NULL,
+--     FOREIGN KEY(team_id) REFERENCES teams(t_id)
+-- );
