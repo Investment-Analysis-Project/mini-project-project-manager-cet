a6c84f060603ab6bb9f46f9b657f743fe4663968
Govind V R <govindvr02@gmail.com>
2023-06-14 11:05:36 +0530

    feat: Endpoint for file upload

--- ml_api/app.py
+++ ml_api/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello, World!"
+
+@app.route("/predict/pdf", methods=["POST"])
+def predict_pdf():
+    if request.method == "POST":
+        file_path = "temp_upload/uploaded_file.pdf"
+        if 'file' not in request.files:
+            return "No file uploaded."
+
+        file = request.files['file']
+        if file.filename == '':
+            return "No file selected."
+
+        # Save the file to a desired location
+        file.save(file_path)
+        print("File saved successfully.")
+        os.remove(file_path)
+
+        return "File uploaded successfully."
+    return "Invalid request."
 
 if __name__ == "__main__":
     app.run(debug=True)
