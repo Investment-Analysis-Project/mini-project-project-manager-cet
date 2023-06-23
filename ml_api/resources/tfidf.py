c2474030130613f41e4c2036ef48b11c42fe4034
Govind V R <govindvr02@gmail.com>
2023-06-23 22:44:16 +0530

    fix:  nltk stopwords

--- ml_api/resources/tfidf.py
+++ ml_api/resources/tfidf.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
-from nltk.corpus import stopwords
 import nltk
 import re
-from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.metrics.pairwise import euclidean_distances
 from nltk.tokenize import word_tokenize
 
 def abstract_similarity(text):
@@ -16,30 +14,30 @@ def abstract_similarity(text):
     ----------
     text : str
         The text extracted from the uploaded PDF file.
-        
+
     """
     try:
         documents_df = pd.read_csv('/etc/secrets/abstracts.csv')
     except:
         documents_df = pd.read_csv('resources/abstracts.csv')
 
-    stop_words_l=stopwords.words('english')
+    stop_words_l = nltk.corpus.stopwords.words('english')
 
     documents_df = documents_df.drop(['Timestamp'], axis=1)
-    documents_df = documents_df.rename(columns={'Group Members \n(Name of all members)':'Group Members', 'Course (If Mtech)': 'Course'})
+    documents_df = documents_df.rename(columns={'Group Members \n(Name of all members)': 'Group Members', 'Course (If Mtech)': 'Course'})
     documents_df = documents_df.drop(['Unnamed: 9'], axis=1)
-  
-    documents_cleaned=documents_df.Abstract.apply(lambda x: " ".join(re.sub(r'[^a-zA-Z]',' ',w).lower() for w in x.split() if re.sub(r'[^a-zA-Z]',' ',w).lower() not in stop_words_l) )
-    tfidfvectoriser=TfidfVectorizer()
+
+    documents_cleaned = documents_df.Abstract.apply(lambda x: " ".join(re.sub(r'[^a-zA-Z]', ' ', w).lower() for w in x.split() if re.sub(r'[^a-zA-Z]', ' ', w).lower() not in stop_words_l))
+    tfidfvectoriser = TfidfVectorizer()
     tfidfvectoriser.fit(documents_cleaned)
-    tfidf_vectors=tfidfvectoriser.transform(documents_cleaned)
+    tfidf_vectors = tfidfvectoriser.transform(documents_cleaned)
     query_document = [text]
-    query_p = pd.DataFrame(query_document,columns=['querys'])
-    query_p['processed'] = query_p.querys.apply(lambda x: " ".join(re.sub(r'[^a-zA-Z]',' ',w).lower() for w in x.split() if re.sub(r'[^a-zA-Z]',' ',w).lower() not in stop_words_l) )
+    query_p = pd.DataFrame(query_document, columns=['querys'])
+    query_p['processed'] = query_p.querys.apply(lambda x: " ".join(re.sub(r'[^a-zA-Z]', ' ', w).lower() for w in x.split() if re.sub(r'[^a-zA-Z]', ' ', w).lower() not in stop_words_l))
 
     input_tfidf = tfidfvectoriser.transform(query_p['processed'])
     similarities = cosine_similarity(input_tfidf, tfidf_vectors)
-    
+
     df_dict = {}
     index = 0
 
