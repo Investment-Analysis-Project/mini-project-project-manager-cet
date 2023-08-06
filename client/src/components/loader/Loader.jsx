77346971029f20cd3696700fff6bf048b677c001
Govind V R <govindvr02@gmail.com>
2023-08-07 01:11:50 +0530

    style: Loader Indicator

--- client/src/components/loader/Loader.jsx
+++ client/src/components/loader/Loader.jsx
@@ -2,7 +2,7 @@ import PropagateLoader from "react-spinners/PropagateLoader";
 const Loader = () => {
     return (
         <div className="loader-container">
-            <PropagateLoader color={'#00143d'} loading={true} size={30} />
+            <PropagateLoader color={'#0DAF5E'} loading={true} size={30} />
             {/* <div className="loading-text">Searching Previous Projects</div> */}
         </div>
     );
